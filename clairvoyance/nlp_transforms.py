419c6501a3419231cbcd7cdf6d90114309c11965
paubric <paubric@gmail.com>
2018-04-22 18:02:42 +0300

    string_to_named_entities and string_to_language

--- clairvoyance/nlp_transforms.py
+++ clairvoyance/nlp_transforms.py
@@ -110,3 +110,43 @@ def string_to_sentiment(graph, nodes):
             return (-1, -1)
 
     return transform_results, output_type
+
+def string_to_named_entities(graph, nodes):
+    print('Performing transform string_to_named_entities on nodes: ', nodes)
+
+    input_type = entity_types.string_type()
+    output_type = entity_types.string_type()
+    transform_results = []
+
+    for node in nodes:
+        if (graph.nodes[node]['type'][0] == input_type[0]):
+            client = Algorithmia.client(algorithmia_key)
+            algo = client.algo('StanfordNLP/NamedEntityRecognition/0.2.0')
+            result = algo.pipe(node)
+            result = [item[0] for item in result.result[0]]
+            transform_results.append(result)
+        else:
+            print('Incompatible types')
+            return (-1, -1)
+
+    return transform_results, output_type
+
+def string_to_language(graph, nodes):
+    print('Performing transform string_to_language on nodes: ', nodes)
+
+    input_type = entity_types.string_type()
+    output_type = entity_types.string_type()
+    transform_results = []
+
+    for node in nodes:
+        if (graph.nodes[node]['type'][0] == input_type[0]):
+            client = Algorithmia.client(algorithmia_key)
+            algo = client.algo('nlp/LanguageIdentification/1.0.0')
+            result = algo.pipe(node)
+            print(result.result)
+            transform_results.append(result.result)
+        else:
+            print('Incompatible types')
+            return (-1, -1)
+
+    return transform_results, output_type
--- main.py
+++ main.py
@@ -2,17 +2,20 @@ from clairvoyance import core, nlp_transforms, entity_types
 import matplotlib.pyplot as plt
 import networkx as nx
 
-sentence = 'best data mining framework ever!'
+sentence = 'Jim went to Stanford University, Tom went to the University of Washington. They both work for Microsoft.'
 name = 'Jane'
 G = core.Graph()
 G.add_entity(name, entity_types.name_type())
 G.add_entity(sentence, entity_types.string_type())
 
+G.make_transform([sentence], nlp_transforms.string_to_language)
+"""
 G.make_transform([name], nlp_transforms.string_to_len)
 G.make_transform([sentence], nlp_transforms.string_to_word_count)
 G.make_transform([name], nlp_transforms.string_to_gender)
 G.make_transform([sentence], nlp_transforms.string_to_sentiment)
 G.make_transform([sentence], nlp_transforms.string_to_tags)
+"""
 
 print('\n')
 print(G.graph.nodes())
